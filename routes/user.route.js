00ef067d70ccf0bdb80b77c34b7d1d6413205384
gaurang kathiriya <kathiriyagaurang44@gmail.com>
2022-05-13 15:08:25 +0530

    removed otp path

--- routes/user.route.js
+++ routes/user.route.js
@@ -6,10 +6,10 @@ const upload = require("../middleware/multer");
 
 router.post("/signup", upload.single("profile"), auth_controller.signup);
 router.post("/login", auth_controller.login);
-router.post("/send_otp" ,auth_controller.otpsend);
-router.post("/resend_otp" ,auth_controller.resend_otp);
+// router.post("/send_otp" ,auth_controller.otpsend);
+// router.post("/resend_otp" ,auth_controller.resend_otp);
 router.post("/forgot_password", auth_controller.forgot_password);
-router.post("/verify_otp" ,auth_controller.verify_otp);
+// router.post("/verify_otp" ,auth_controller.verify_otp);
 router.post("/reset_password", verify, auth_controller.reset_password);
 router.put("/change_password", verify, auth_controller.change_password);
 
